8ed90d040df90fdbfe24e31e4fc5aa671b163e2c
Matthew Kight <kightmat@msu.edu>
2020-10-23 10:11:41 -0400

    Initial Commit

---
+++ insta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  insta
+//
+//  Created by Matthew Kight on 10/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
